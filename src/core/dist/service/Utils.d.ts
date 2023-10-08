658f98be88db3930cdcfd1df36343b632450427d
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-10-08 13:54:00 -0500

    fix(core): remove dist folder

--- src/core/dist/service/Utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare function generateNewSessionID(): string;
-declare function genRandomName(): string;
-declare function verifyAuth(signStr: any, streamId: any, secretKey: any): boolean;
-declare function getFFmpegVersion(ffpath: any): Promise<unknown>;
-declare function getFFmpegUrl(): string;
-export { generateNewSessionID, verifyAuth, genRandomName, getFFmpegVersion, getFFmpegUrl, };
